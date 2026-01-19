57bd161640742217792de2f311f1e8198aa57410
Lovejeet Patel <love.jeet052004@gmail.com>
2026-01-19 13:26:21 +0530

    Add files via upload

---
+++ FSD2_1-main/FSD2_1-main/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter SPA</h1>
+      <h2>Counter Value: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
