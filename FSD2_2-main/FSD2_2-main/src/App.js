f3199f3f345eaf76e4897fd17ddecf727f0772df
Lovejeet Patel <love.jeet052004@gmail.com>
2026-01-19 13:43:39 +0530

    Add files via upload

---
+++ FSD2_2-main/FSD2_2-main/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple To-Do List SPA</h1>
+
+      <input
+        type="text"
+        value={task}
+        placeholder="Enter task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask} style={{ marginLeft: "10px" }}>
+        Add
+      </button>
+
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t}
+            <button
+              onClick={() => deleteTask(index)}
+              style={{ marginLeft: "10px" }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
